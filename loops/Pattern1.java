4fc98f53f3b60e9d160b05f208b19399d974a64f
Ankit Tiwari <ankyofficial9099@gmail.com>
2026-01-17 16:20:27 +0530

    create new file

---
+++ loops/Pattern1.java
@@ -0,0 +1,43 @@
+public class Pattern1{
+    public static void main(String[] args){
+                  
+      //print solid rectngle -----------------
+
+      /*  for(int i=1;i<=4; i++){
+            for(int j=1; j<=5; j++){
+                System.out.print("*");
+            }
+            System.out.println();   
+        }
+
+        //print hollow rectangle-----------------
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=5; j++){
+                if(i==1||j==1||i==4||j==5)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+       //print half pyramid-----------------
+       for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+           System.out.print("*");
+        }
+        System.out.println();
+       }
+
+      //print reverse half pyramid--------------
+       for(int i=5; i>=1; i--){
+        for(int j=1; j<=i; j++){
+           System.out.print("*");
+        }
+        System.out.println();
+       } */
+      //print full pyramid-----------------
+      
+
+
+}}
